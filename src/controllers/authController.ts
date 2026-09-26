3e13e7ec95b9ea33cdf654750eeaaec6c0ff8533
rek75625 <rekhan7562@gmail.com>
2026-09-26 17:31:03 +0500

    updated code for login and register api

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,7 +1,7 @@
 import type { Request, Response } from "express";
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import pool from "../models/db.js";
-import type { Jwt } from "jsonwebtoken";
 
 
 
@@ -21,128 +21,42 @@ export const register = async(req: Request,res:Response)=>{
 
 
     } catch(error){
-         res.status(500).json({message: "User failed to register"});
+         res.status(500).json({error: "User failed to register"});
     }
     
 }
 
+/// login function 
+export const login = async(req: Request,res:Response):Promise<any>=>{
 
-export const register1 = async(req: Request,res:Response)=>{
-
-    const {username, email, password} = req.body;
+    const {email,password} = req.body;
     try{
-        const hashedPassword = await bcrypt.hash(password,SALT_ROUND);
-        const result = await pool.query("INSERT INTO users (username, email, password) VALUES (s1, s2, s3) RETURNING *",
-            [username, email, hashedPassword]
+
+         const result = await pool.query("SELECT * FROM users WHERE email = s2",
+            [email]
         );
+
         const user = result.row[0];
-        res.status(201).json({message: "User registered successfully",user});
+        if(!user) return res.status(404).json({error: "User not found"});
 
+        const isMatch = await bcrypt.compare(password,user.password);
+        if(!isMatch) return res.status(400).json({error: "Invalid credentials"})
 
-    } catch(error){
-         res.status(500).json({message: "User failed to register"});
+        const token = jwt.sign(
+            { id: user.id},
+            JWT_SECRET,
+            { expiresIn: '10h' }
+        );
+        res.json({ message: "Login successful", token });
+
+    } catch(error)
+    
+    {
+          res.status(400).json({error: "User failed to login"})
     }
     
+
 }
-export const register2 = async(req: Request,res:Response)=>{
-
-    const {username, email, password} = req.body;
-    try{
-        const hashedPassword = await bcrypt.hash(password,SALT_ROUND);
-        const result = await pool.query("INSERT INTO users (username, email, password) VALUES (s1, s2, s3) RETURNING *",
-            [username, email, hashedPassword]
-        );
-        const user = result.row[0];
-        res.status(201).json({message: "User registered successfully",user});
 
 
-    } catch(error){
-         res.status(500).json({message: "User failed to register"});
-    }
-    
-}
-export const register3 = async(req: Request,res:Response)=>{
 
-    const {username, email, password} = req.body;
-    try{
-        const hashedPassword = await bcrypt.hash(password,SALT_ROUND);
-        const result = await pool.query("INSERT INTO users (username, email, password) VALUES (s1, s2, s3) RETURNING *",
-            [username, email, hashedPassword]
-        );
-        const user = result.row[0];
-        res.status(201).json({message: "User registered successfully",user});
-
-
-    } catch(error){
-         res.status(500).json({message: "User failed to register"});
-    }
-    
-}
-export const register4 = async(req: Request,res:Response)=>{
-
-    const {username, email, password} = req.body;
-    try{
-        const hashedPassword = await bcrypt.hash(password,SALT_ROUND);
-        const result = await pool.query("INSERT INTO users (username, email, password) VALUES (s1, s2, s3) RETURNING *",
-            [username, email, hashedPassword]
-        );
-        const user = result.row[0];
-        res.status(201).json({message: "User registered successfully",user});
-
-
-    } catch(error){
-         res.status(500).json({message: "User failed to register"});
-    }
-    
-}
-export const register5 = async(req: Request,res:Response)=>{
-
-    const {username, email, password} = req.body;
-    try{
-        const hashedPassword = await bcrypt.hash(password,SALT_ROUND);
-        const result = await pool.query("INSERT INTO users (username, email, password) VALUES (s1, s2, s3) RETURNING *",
-            [username, email, hashedPassword]
-        );
-        const user = result.row[0];
-        res.status(201).json({message: "User registered successfully",user});
-
-
-    } catch(error){
-         res.status(500).json({message: "User failed to register"});
-    }
-    
-}
-export const register6 = async(req: Request,res:Response)=>{
-
-    const {username, email, password} = req.body;
-    try{
-        const hashedPassword = await bcrypt.hash(password,SALT_ROUND);
-        const result = await pool.query("INSERT INTO users (username, email, password) VALUES (s1, s2, s3) RETURNING *",
-            [username, email, hashedPassword]
-        );
-        const user = result.row[0];
-        res.status(201).json({message: "User registered successfully",user});
-
-
-    } catch(error){
-         res.status(500).json({message: "User failed to register"});
-    }
-    
-}
-export const register7 = async(req: Request,res:Response)=>{
-
-    const {username, email, password} = req.body;
-    try{
-        const hashedPassword = await bcrypt.hash(password,SALT_ROUND);
-        const result = await pool.query("INSERT INTO users (username, email, password) VALUES (s1, s2, s3) RETURNING *",
-            [username, email, hashedPassword]
-        );
-        const user = result.row[0];
-        res.status(201).json({message: "User registered successfully",user});
-
-
-    } catch(error){
-         res.status(500).json({message: "User failed to register"});
-    }
-    
-}
--- src/models/db.ts
+++ src/models/db.ts
@@ -4,67 +4,10 @@ import {Pool} from 'pg';
 
 const pool = new Pool({
     user : 'postgres',
-    password:'admin',
+    password:'REk!2345',
     host:'localhost',
     port: 5432,
-    database:'woridb'
-})
-
-////  These all pools are extras only above one is good
-const pool1 = new Pool({
-    user : 'postgres',
-    password:'admin',
-    host:'localhost',
-    port: 5432,
-    database:'woridb'
-})
-const pool2 = new Pool({
-    user : 'postgres',
-    password:'admin',
-    host:'localhost',
-    port: 5432,
-    database:'woridb'
-})
-const pool3 = new Pool({
-    user : 'postgres',
-    password:'admin',
-    host:'localhost',
-    port: 5432,
-    database:'woridb'
-})
-const pool4 = new Pool({
-    user : 'postgres',
-    password:'admin',
-    host:'localhost',
-    port: 5432,
-    database:'woridb'
-})
-const pool5 = new Pool({
-    user : 'postgres',
-    password:'admin',
-    host:'localhost',
-    port: 5432,
-    database:'woridb'
-})
-const pool6 = new Pool({
-    user : 'postgres',
-    password:'admin',
-    host:'localhost',
-    port: 5432,
-    database:'woridb'
-})
-const pool7 = new Pool({
-    user : 'postgres',
-    password:'admin',
-    host:'localhost',
-    port: 5432,
-    database:'woridb'
+    database:'postgres'
 })
 
 export default pool;
-// export default pool2;
-// export default pool3;
-// export default pool4;
-// export default pool5;
-// export default pool6;
-// export default pool7;
